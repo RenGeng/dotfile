f1cd34af8007b9a142511ed6ae3390a67cd1dd0c
Geng REN <geng.ren@ext.groupepvcp.com>
2026-01-09 17:48:07 +0100

    added colorize and new colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,39 +1,39 @@
-return {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme cyberdream")
-
-        require("cyberdream").setup({
-            -- Enable transparent background
-            transparent = false,
-
-            -- Enable italics comments
-            italic_comments = true,
-
-            -- Replace all fillchars with ' ' for the ultimate clean look
-            hide_fillchars = false,
-
-            -- Modern borderless telescope theme - also applies to fzf-lua
-            borderless_telescope = true,
-
-            -- Set terminal colors used in `:terminal`
-            terminal_colors = true,
-
-            -- Use caching to improve performance - WARNING: experimental feature - expect the unexpected!
-            -- Early testing shows a 60-70% improvement in startup time. YMMV. Disables dynamic light/dark theme switching.
-            cache = false, -- generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
-
-            -- Disable or enable colorscheme extensions
-            extensions = {
-                telescope = true,
-                notify = true,
-            },
-        })
-    end,
-}
-
+-- return {
+--     "scottmckendry/cyberdream.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.cmd("colorscheme cyberdream")
+--
+--         require("cyberdream").setup({
+--             -- Enable transparent background
+--             transparent = false,
+--
+--             -- Enable italics comments
+--             italic_comments = true,
+--
+--             -- Replace all fillchars with ' ' for the ultimate clean look
+--             hide_fillchars = false,
+--
+--             -- Modern borderless telescope theme - also applies to fzf-lua
+--             borderless_telescope = true,
+--
+--             -- Set terminal colors used in `:terminal`
+--             terminal_colors = true,
+--
+--             -- Use caching to improve performance - WARNING: experimental feature - expect the unexpected!
+--             -- Early testing shows a 60-70% improvement in startup time. YMMV. Disables dynamic light/dark theme switching.
+--             cache = false, -- generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+--
+--             -- Disable or enable colorscheme extensions
+--             extensions = {
+--                 telescope = true,
+--                 notify = true,
+--             },
+--         })
+--     end,
+-- }
+--
 
 -- return {
 --     'navarasu/onedark.nvim',
@@ -63,3 +63,26 @@ return {
 --         vim.cmd.colorscheme("oasis") -- After setup, apply theme (or a any style like "oasis-night")
 --     end
 -- }
+--
+--
+--
+--
+-- return {
+--     "nyoom-engineering/oxocarbon.nvim",
+--     config = function()
+--         vim.opt.background = "dark"
+--         vim.cmd.colorscheme "oxocarbon"
+--     end
+-- }
+--
+return {
+    "serhez/teide.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+        vim.cmd([[colorscheme teide]])
+        require("teide").setup({ style = "darker", })
+    end
+
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,46 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "bash",
-        "lua",
-        "git_config",
-        "git_rebase",
-        "go",
-        "json",
-        "python",
-        "sql",
-        "terraform",
-        "csv",
-        "yaml",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-      },
-    })
-  end,
+        -- configure treesitter
+        treesitter.setup({ -- enable syntax highlighting
+            highlight = {
+                enable = true,
+            },
+            -- enable indentation
+            indent = { enable = true },
+            -- enable autotagging (w/ nvim-ts-autotag plugin)
+            autotag = {
+                enable = true,
+            },
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "bash",
+                "lua",
+                "git_config",
+                "git_rebase",
+                "go",
+                "json",
+                "python",
+                "sql",
+                "terraform",
+                "csv",
+                "yaml",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "query",
+                "vimdoc",
+                "c",
+            },
+        })
+    end,
 }
